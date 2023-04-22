380f5cc7a2c445358cd996bbf417f1b7dff4ee9f
mladenkg123 <cvetkovicmladen00@gmail.com>
2023-04-22 22:03:38 +0200

    Navbar + Header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import { Login } from "./Login";
 import { Register } from "./Register";
-
+import { Navbar } from "./components";
+import { Header } from "./containers";
+ 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
 
@@ -12,11 +14,16 @@ function App() {
   }
 
   return (
-    <div className="App">
+   
+   <div className="App">
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
       {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+        // currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
       }
-    </div>
+     </div> 
   );
 }
 
