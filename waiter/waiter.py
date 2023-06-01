a95c756a6b850d6fce2e29d1264ae7ef8e440737
Djordje <djordje00karisic@gmail.com>
2023-06-01 20:56:16 +0200

    fix
    xif

--- waiter/waiter.py
+++ waiter/waiter.py
@@ -7,14 +7,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 CORS(app)
-
+global context
+context=""
 client = pymongo.MongoClient("localhost", 27017, maxPoolSize=50)
 db = client["GPTDB"]
 collection = db['prompts']
 cursor = collection.find()
-#prompt_list = []
-#answer_list = []
-'''
+prompt_list = []
+answer_list = []
+
 for document in cursor:
     prompt_list.append(document['prompt'])
 collection = db['answers']
@@ -22,16 +23,23 @@ cursor = collection.find()
 for document in cursor:
     answer_list.append(document['answer'])
 for i, prompt in enumerate(prompt_list):
-    context += "\r\n ME: " + prompt + "\n" + "\r\nLearnGPT: " + answer_list[i] + "\n"
-'''
+    context += "\r\n USER: " + prompt + "\n" + "\r\nLearnGPT: " + answer_list[i] + "\n"
+    
+    
+
+
+    
+print(context)
+
 @app.route('/', methods=['POST'])
 def handle_post():
     data = request.get_json()  # Get the JSON data from the request
     # Process the data or perform any necessary actions
     # You can access specific fields of the JSON data using data['field_name']
     waiter = Waiter()
-    context = "ME:Pretend you're an enthusiastic english teacher. Do not break character or speak in any other language than english. Correct my grammar mistakes and talk with me about things, and freely ask any questions.\n ChatGPT:Ok.\n"
-    r = waiter.request(context + " " + data['prompt'] + "\n")
+    global context
+    print(context)
+    r = waiter.request(context + "\n" + data['prompt'] + "\n")
     # Return a response
     response = {'message': 'POST request received', 'data': r}
     return response, 200
@@ -50,14 +58,14 @@ class Waiter:
         url = self.getURL()
         payload = {
             "model":"gpt-3.5-turbo",
-            "messages": [{"role": "user","content":wish},
+            "messages": [
                          {
           "role": "system",
-          "content": "You're an enthusiastic English teacher who likes to present himself in a fun way. Your nickname is Waiter. Only greet student once. Do not break character or speak in any other language than English. \
+          "content": "You're an enthusiastic English teacher who likes to present himself in a fun way. Your nickname is LearnAI. Only greet student once. Do not break character or speak in any other language than English. \
             Initiate conversations, give user assignments. Focus on conversating with your students. Your lectures are only 1 on 1.\
            and ask questions. Recommend a topic if there isn't one present. Occasionally ask user to complete or correct a sentence or a word gramatically.\
-             Always correct user's grammatical and other errors. When recommending adjectives, provide their definition."
-       }]
+             Always correct user's grammatical and other errors. When recommending adjectives, provide their definition. Pay attention to the history that is fed to you."
+       },{"role": "user","content":wish}]
             
         }
         headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
